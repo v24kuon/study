3b8c249732eb333b09ab03822e0d168dc8240c25
vagrant <vagrant@localhost.localdomain>
2020-07-30 13:01:07 +0000

    コメント

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,24 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   def create
-    @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
-    @comment.user_id = current_user.id
-    if @comment.save
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.user_id = current_user.id
+    if comment.save
+      @comments = post.comments
+      @comments_count = @comments.count
+      @comments_count_class = (@comments_count == 0)? "comments-none" : "comments-exist"
       render :index
     end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @post = Post.find(params[:post_id])
-    if @comment.destroy
+    comment = Comment.find(params[:id])
+    post = Post.find(params[:post_id])
+    if comment.destroy
+      @comments = post.comments
+      @comments_count = @comments.count
+      @comments_count_class = (@comments_count == 0)? "comments-none" : "comments-exist"
       render :index
     end
   end
@@ -21,4 +27,5 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:content, :post_id, :user_id)
     end
+
 end
